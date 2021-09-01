bba016e07e0d6cce84e45008b0195fa790caff95
chandrasekaran1542000 <chandrasekaran1542000@gmail.com>
2021-09-01 15:37:55 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  isOn:boolean=false;
+  check(){
+    this.isOn=!this.isOn;
+    console.log(this.isOn);
+  }
+
 }
